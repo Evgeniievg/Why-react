b6e371e085bf8fb4cf89a716f7d7de1f1041c622
Evgeniievg <evgeniy.litvinv01@gmail.com>
2023-01-12 20:08:49 +0400

    imagee fixed

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -5,7 +5,7 @@ export default function Navbar() {
     <header className='header'>
       <div className='header__wrapper'>
         <div className='header__left'>
-          <img src='/logo512.png' className="header__image" />
+          <img src='./logo512.png' className="header__image" />
           <h3 className='header__left_text'>ReactFacts</h3>
         </div>
         <h4 className='header__text'>React Course - Project 1</h4>
